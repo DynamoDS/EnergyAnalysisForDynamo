e83e41681656da6c746efdec4299a81201decdce
Benjamin Howes <bhowes@thorntontomasetti.com>
2015-06-05 15:01:14 -0400

    Tried to get a RTF hello world to work
    no dice :disappointed:

---
+++ src/EnergyAnalysisForDynamo_TESTING/RTFtest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RTFtest1 : RevitSystemTestBase
+    {
+        //[Test, TestModel(@".\EnergyAnalysisForDynamo_ex1_simpleRevitMass.rvt")]
+        [Test]
+        public void HelloWorld()
+        {
+            //OpenSampleDefinition(@"EnergyAnalysisForDynamo_ex1b_CreateEnergyModelAndSetSurfaceParams.dyn");
+
+            Assert.Pass();
+        }
+    }
+}
